395b3ac4bbf33c12304c07fe853d7990b6b4240d
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-19 18:28:03 +0300

    created login view function and loged in the user if the user credentials are true

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from ast import Pass
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .models import UserAccount
 
 # Create your views here.
 def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else :
+            return JsonResponse({"User": "invalid user"})
     return render(request, 'accounts/login.html')
