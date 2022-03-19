31e2c968bcfd62f53a9c54467a4287293274a999
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-19 18:19:15 +0300

    created login page with two fields password and email.

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('neighbour.urls'))
+    path('', include('neighbour.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
