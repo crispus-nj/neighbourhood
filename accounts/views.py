6019f92c4fda0b24e71435f812b8abd22f38a5f0
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-19 19:45:36 +0300

    created the register view functionality and logged in the user upon successfull registering

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,31 @@ from .forms import RegistrationForm
 
 # Create your views here.
 def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            password = form.cleaned_data['password']
+            phone_number = form.cleaned_data['phone_number']
+            username = email.split("@")[0]
+
+            user = UserAccount.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                username = username,
+                password = password
+            )
+            user.phone_number = phone_number
+            user.is_active = True
+            user.save()
+
+            return redirect('login')
+        else :
+            return JsonResponse({"User": "invalid user"}) 
+
     form = RegistrationForm()
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
