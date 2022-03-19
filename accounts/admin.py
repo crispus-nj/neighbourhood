5b81edcb4fd5859a63e34beff427eac5d2fca708
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-19 15:55:42 +0300

    added new app neighbour and registered the app to the installed apps. added path to the project urls

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,9 +5,11 @@ from .models import UserAccount
 
 
 class StyleUser(UserAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'email', 'is_admin', 'is_active')
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_admin', 'is_active', 'last_login')
     list_display_links = ('username', 'email')
     filter_horizontal = ()
     fieldsets = ()
+    list_filter = ()
+    ordering = ('-last_login', )
 
 admin.site.register(UserAccount, StyleUser)
--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'neighbour',
 ]
 
 AUTH_USER_MODEL = 'accounts.UserAccount'
@@ -57,7 +58,9 @@ ROOT_URLCONF = 'neighbourhood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATICFILES_DIRS = [ BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
