fd6418d3793a3a50fac64b5a82a7910eae87f25a
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-19 22:47:30 +0300

    created post, location and Business model in the neighbour application

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from accounts.models import UserAccount
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    image = models.ImageField()
+    description = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Location(models.Model):
+    name = models.CharField(max_length=70)
+    city = models.CharField(max_length=80)
+    people = models.ManyToManyField(UserAccount, blank=True, null=True, related_name='people')
+
+    def __str__(self):
+        return self.name
+
+class Business(models.Model):
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    loaction = models.ForeignKey(Location, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
