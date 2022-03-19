fdcda5e163c96fb27a9f8981eab1f11e671f34f7
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-19 15:18:32 +0300

    deleted user app due to name crash, added accounts app and added custome user model

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'userAccount'
+    'accounts'
 ]
+
+AUTH_USER_MODEL = 'accounts.UserAccount'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
