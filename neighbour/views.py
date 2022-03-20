f68ba4a7574ea766c10547de7bdf40af4d930fd6
engineer237 <crispus.muthiora@student.moringaschool.com>
2022-03-20 10:46:08 +0300

    in the business view function, i added logic to add business to the database through the form inside the modal

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Business, Location, Post
 from .forms import BusinessRegistrationForm
@@ -13,6 +13,30 @@ def landing(request):
 @login_required(login_url='login')
 def business(request):
     businesses = Business.objects.all()
+
+    if request.method == 'POST':
+        form = BusinessRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            category = form.cleaned_data['category']
+            image = form.cleaned_data['image']
+            location = form.cleaned_data['location']
+            user = request.user
+
+            print(name, category, image, location, user)
+
+            business = Business.objects.create(
+                name = name,
+                category = category,
+                image = image,
+                location = location,
+                user = user
+            )
+            business.save()
+
+            return redirect('business')
+
+
     form = BusinessRegistrationForm()
     context = {'businesses': businesses, 'form':form}
 
